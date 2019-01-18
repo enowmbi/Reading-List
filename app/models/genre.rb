e1b13f5c3e29ca76c70f0206835595f50263cf5e
Enow Mbi <benowmbi@yahoo.com>
2019-01-18 02:47:45 +0100

    add genre model spec-validating name

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
 has_many :books
+
+validates :name,presence: true
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    let(:genre){Genre.new(name: 'Programming')}
+    it 'is invalid without name' do 
+      genre.name =""
+      expect(genre).not_to be_valid
+    end
+  end
 end
