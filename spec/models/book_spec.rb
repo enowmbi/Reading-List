8baee28d79870274734dcc8c7acccc3f40b77d15
Enow Mbi <benowmbi@yahoo.com>
2020-01-10 17:09:53 +0100

    Modify books spec -- updated paths

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,4 @@
-
+require 'rails_helper'
 RSpec.describe Book, type: :model do
   let(:book){Book.new(title:"my title",rating:4,finished_at:1.day.ago)}
   context "validation" do 
@@ -7,7 +7,5 @@ RSpec.describe Book, type: :model do
       book.title = ""
       expect(book).not_to be_valid
     end
-
-  
   end
 end
