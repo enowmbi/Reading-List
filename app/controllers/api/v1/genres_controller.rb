37324c3638f1291a4559989e99fb61448b53ef02
Enow Mbi <benowmbi@yahoo.com>
2020-01-10 17:09:05 +0100

    Modify genres controller --added modules Api::V1::

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,45 +1,50 @@
-class Api::V1::GenresController < ApplicationController
-  before_action :set_genre, only: [:show,:update, :destroy]
+module Api
+  module V1
+    class GenresController < ApplicationController
+      before_action :set_genre, only: [:show,:update, :destroy]
 
-  def index
-    genres = Genre.all
-    render json: genres
-  end
+      def index
+        genres = Genre.all
+        render json: genres
+      end
 
-  def show
-    render json: @genre,include: ['books']
-  end
+      def show
+        render json: @genre,include: ['books']
+      end
 
-  def create
-    genre = Genre.new(genre_params)
-    if genre.save
-      render json: genre, status: :created
-    else
-      render json: genre.errors, status: :unprocessable_entity
+      def create
+        genre = Genre.new(genre_params)
+        if genre.save
+          render json: genre, status: :created
+        else
+          render json: genre.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @genre.update(genre_params) 
+          render json: @genre
+        else
+          render json: @genre.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if !@genre.destroy
+          render json: @genre.errors, status: :unprocessable_entity
+        end
+      end
+
+      private 
+      def set_genre
+        @genre = Genre.find(params[:id])
+      end
+
+      def genre_params
+        params.require(:genre).permit(:name)
+      end
+
     end
   end
-
-  def update
-    if @genre.update(genre_params) 
-      render json: @genre
-    else
-      render json: @genre.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if !@genre.destroy
-      render json: @genre.errors, status: :unprocessable_entity
-    end
-  end
-
-  private 
-  def set_genre
-    @genre = Genre.find(params[:id])
-  end
-
-  def genre_params
-    params.require(:genre).permit(:name)
-  end
-
 end
+
