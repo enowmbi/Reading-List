34d659122291a9d40463b0afaa84534e5fc9af5c
Enow Mbi <benowmbi@yahoo.com>
2020-01-17 18:29:35 +0100

    Add redirect to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/', to: redirect('/api/v1/books')
+  get '/books', to: redirect('/api/v1/books')
+  get '/books/:id', to: redirect('/api/v1/books/#{id}')
+  get '/genres', to: redirect('/api/v1/genres')
+  get '/genres/:id', to: redirect('/api/v1/genres/#{id}')
   namespace :api do 
     namespace :v1 do 
       root 'books#index'
