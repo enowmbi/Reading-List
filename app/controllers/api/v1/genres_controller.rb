93b9774e8e2689dc2f4e5ab427d75f9de3784d4d
Enow Mbi <benowmbi@yahoo.com>
2021-10-21 07:24:59 +0100

    Refactor genres controller

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,41 +1,43 @@
+# frozen_string_literal: true
+
 module Api
   module V1
+    # Genre controller
     class GenresController < ApplicationController
-      before_action :set_genre, only: [:show,:update, :destroy]
+      before_action :set_genre, only: %i[show update destroy]
 
       def index
         genres = Genre.all
-        render json: genres
+        render(json: genres)
       end
 
       def show
-        render json: @genre,include: ['books']
+        render(json: @genre, include: ['books'])
       end
 
       def create
         genre = Genre.new(genre_params)
         if genre.save
-          render json: genre, status: :created
+          render(json: genre, status: :created)
         else
-          render json: genre.errors, status: :unprocessable_entity
+          render(json: genre.errors, status: :unprocessable_entity)
         end
       end
 
       def update
-        if @genre.update(genre_params) 
-          render json: @genre
+        if @genre.update(genre_params)
+          render(json: @genre)
         else
-          render json: @genre.errors, status: :unprocessable_entity
+          render(json: @genre.errors, status: :unprocessable_entity)
         end
       end
 
       def destroy
-        if !@genre.destroy
-          render json: @genre.errors, status: :unprocessable_entity
-        end
+        render(json: @genre.errors, status: :unprocessable_entity) unless @genre.destroy
       end
 
-      private 
+      private
+
       def set_genre
         @genre = Genre.find(params[:id])
       end
@@ -43,7 +45,6 @@ module Api
       def genre_params
         params.require(:genre).permit(:name)
       end
-
     end
   end
 end
