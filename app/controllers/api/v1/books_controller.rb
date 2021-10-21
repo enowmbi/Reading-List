87cb9df7834be83a46da6fca3b2f2b4d0319ae83
Enow Mbi <benowmbi@yahoo.com>
2021-10-21 07:17:50 +0100

    Refactor books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,55 +1,55 @@
+# frozen_string_literal: true
+
 module Api
   module V1
+    # Book controller
     class BooksController < ApplicationController
-      before_action :set_book, only: [:show, :update, :destroy]
+      before_action :set_book, only: %i[show update destroy]
 
-      def index 
-        if rating = params[:rating]
-          books = Book.where(rating: rating)
-        else
-          books = Book.all
-        end
+      def index
+        books = if rating == params[:rating]
+                  Book.where(rating: rating)
+                else
+                  Book.all
+                end
 
-        render json: books, status: :ok 
+        render(json: books, status: :ok)
       end
 
       def finished
         books = Book.finished
-        render json: books
+        render(json: books)
       end
 
       def show
-        render json: @book 
+        render(json: @book)
       end
 
       def create
         book = Book.new(book_params)
         if book.save
-          render json: book, status: :created
+          render(json: book, status: :created)
         else
-          render json: book.errors, status: :unprocessable_entity
+          render(json: book.errors, status: :unprocessable_entity)
         end
       end
 
       def update
-        if !@book.update(book_params)
-          render json: @book.errors, status: :unprocessable_entity
-        end
+        render(json: @book.errors, status: :unprocessable_entity) unless @book.update(book_params)
       end
 
       def destroy
-        if !@book.destroy
-          render json: @book.errors, status: :unprocessable_entity
-        end
+        render(json: @book.errors, status: :unprocessable_entity) unless @book.destroy
       end
 
       private
+
       def set_book
         @book = Book.find(params[:id])
       end
 
       def book_params
-        params.require(:book).permit(:title,:rating,:finished_at,:genre_id)
+        params.require(:book).permit(:title, :rating, :finished_at, :genre_id)
       end
     end
   end
