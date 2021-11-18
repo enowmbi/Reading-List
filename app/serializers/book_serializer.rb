b8c99297f089b766f7071cdf430fb92795c5e9b0
Enow Mbi <benowmbi@yahoo.com>
2021-11-18 01:08:37 +0100

    Fix rubocop offences -- Add comments; frozen_string_literal and comment classes

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# book serializer class
 class BookSerializer < ActiveModel::Serializer
-  attributes :id,:title,:rating,:finished_at
+  attributes :id, :title, :rating, :finished_at
 
   belongs_to :genre
 end
--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# genre serializer
 class GenreSerializer < ActiveModel::Serializer
   attributes :id, :name
 
